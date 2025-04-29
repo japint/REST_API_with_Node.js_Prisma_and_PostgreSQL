e6ecae573682e8967ab00a7d4bc898dd131ad893
meistergpc <jap1centeno@gmail.com>
2025-04-29 18:33:34 +0200

    Added delete

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -5,5 +5,6 @@ router.post("/", categoryController.createCategory);
 router.get("/", categoryController.getCategories);
 router.get("/:id", categoryController.getCategoryById);
 router.put("/:id", categoryController.updateCategory);
+router.delete("/:id", categoryController.deleteCategory);
 
 module.exports = router;
